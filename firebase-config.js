a0dff63cbc02d619e0f1452b2c967d2d672ee2a6
pedrocavalcant159-afk <pedrocavalcant159@gmail.com>
2026-06-22 11:27:11 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.POST_PREVIEW_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAdHnFNttbFvYjKYDU_qy5KuSKVb5BQoHY",
+  authDomain: "postpreview-aaefc.firebaseapp.com",
+  projectId: "postpreview-aaefc",
+  storageBucket: "postpreview-aaefc.firebasestorage.app",
+  messagingSenderId: "150839925347",
+  appId: "1:150839925347:web:e67f1ac822fd656ff43656"
+};
